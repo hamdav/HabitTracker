0160391af54e72a74dd92249901a20c50b2f8f44
David Hambraeus <Ddhambraeus@gmail.com>
2023-01-02 18:37:22 +0100

    Added (re)naming of habits and datepicker on longpress

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+enum HabitAction { rename, editCategories, removeLastCheck, delete, }
+
 void main() {
   runApp(const MyApp());
 }
@@ -42,72 +44,153 @@ class _RandomNumberState extends State<RandomNumber> {
 		yield rng.nextInt(100);
 	}
 	final _biggerFont = const TextStyle(fontSize: 18);
+	String? codeDialog;
+	String? valueText;
 
 	@override
 	Widget build(BuildContext context) {
 		final rng = Random();
 
-		_habits.add(Habit(_rndGenerator(rng).first.toString()));
+		HabitAction? menuSelection;
 
 		var habitWidgets = _habits.map(
 			(h) => ListTile(
 				title: Text(h.name,
 					style: _biggerFont,
 				),
-				trailing: Icon(
+				subtitle: Text(h.checks.toString()),
+				leading: const Icon(
 					Icons.check_circle,
 					color: Colors.green,
 					semanticLabel: 'Do',
 					size: 40
 				  ),
-			)
-		).toList();
+				onTap: () {
+					setState(() {h.check();});
+				},
+				onLongPress: () {
+					showDatePicker(
+						initialDate: DateTime.now(),
+						firstDate: DateTime.utc(1979,1,1),
+						lastDate: DateTime.utc(3000,1,1),
+						context: context,
+					).then((date) => setState(()=> h.check(date)));
+				},
+				trailing: PopupMenuButton<HabitAction>(
+					initialValue: menuSelection,
+					itemBuilder: (context) =>
+						<PopupMenuEntry<HabitAction>>[
+							const PopupMenuItem<HabitAction>(
+								child: Text('Rename'),
+								value: HabitAction.rename,
+							),
+							const PopupMenuItem<HabitAction>(
+								child: Text('Edit categories'),
+								value: HabitAction.editCategories,
+							),
+							const PopupMenuItem<HabitAction>(
+								child: Text('Remove last check'),
+								value: HabitAction.removeLastCheck,
+							),
+							const PopupMenuItem<HabitAction>(
+								child: Text('Delete',
+									style: const TextStyle(
+										fontWeight: FontWeight.bold,
+										color: Colors.red)),
+								value: HabitAction.delete,
+							),
+						],
+					onSelected: (HabitAction selection) {
+						setState(() {
+							switch (selection) {
+								case HabitAction.delete:
+									// TODO: Confirm?
+									_habits.remove(h);
+									break;
+								case HabitAction.rename:
+									_displayTextInputDialog(context, "Rename", "New name")
+										.then((newName) {h.name = newName ?? h.name;});
+									break;
+								default:
+									print("HI");
+							}
+						});
+						},
+					),
+				)
+			).toList();
 
 		return Scaffold(
 			body: ListView(
 					children: habitWidgets
 				),
 			floatingActionButton: FloatingActionButton(
-					child: Icon(Icons.add),
-					onPressed: () {
-						setState(() {_habits.add(Habit(_rndGenerator(rng).first.toString())); });
-					},
-				),
-			);
-		/* return ListView.builder( */
-		/* 	padding: const EdgeInsets.all(16.0), */
-		/* 	itemBuilder: (context, i) { */
-		/* 		if (i.isOdd) return const Divider(); */
-
-		/* 		final index = i ~/ 2; */
-		/* 		if (index >= _numbers.length) { */
-		/* 			_numbers.addAll(_rndGenerator(rng).take(10)); */
-		/* 		} */
-		/* 		return ListTile( */
-		/* 			title: Text(_numbers[index].toString(), */
-		/* 				style: _biggerFont, */
-		/* 			), */
-		/* 			trailing: Icon( */
-		/* 				Icons.check_circle, */
-		/* 				color: Colors.green, */
-		/* 				semanticLabel: 'Do', */
-		/* 				size: 40 */
-		/* 			  ), */
-		/* 		); */
-		/* 	}, */
-		/* ); */
+				child: const Icon(Icons.add),
+				onPressed: () {
+					_displayTextInputDialog(context, "Name of habit", "Name")
+						.then((name) {
+							if (name != null) {
+								setState(() {_habits.add(Habit(name)); });
+							}
+						});
+				},
+			),
+		);
+		}
+	Future<String?> _displayTextInputDialog(BuildContext context, String title, String hint) async {
+		/*
+		 * Shows a popup with input and returns the user input if
+		 * they pressed OK, otherwise it returns null
+		 */
+		String? tmpText;
+		String? returnText;
+		await showDialog(
+			context: context,
+			builder: (context) {
+				return AlertDialog(
+					title: Text(title),
+					content: TextField(
+						onChanged: (value) {
+							setState(() { tmpText = value; });
+						},
+						decoration: InputDecoration(hintText: hint),
+					),
+					actions: <Widget>[
+						TextButton(
+							child: Text('CANCEL'),
+							onPressed: () {
+								setState(() {
+									returnText = null;
+									Navigator.pop(context);
+								});
+							},
+						),
+						TextButton(
+							child: Text('OK'),
+							onPressed: () {
+								setState(() {
+									returnText = tmpText;
+									Navigator.pop(context);
+								});
+							},
+						),
+					],
+				);
+			}
+		);
+		return returnText;
 	}
 }
 
 class Habit {
-	late final String name;
+	late String name;
 	Set<Habit> categories = {};
 	late List<DateTime> checks = [];
 
 	Habit(this.name);
 
-	void check() {
-		checks.add(DateTime.now());
+	void check([DateTime? d]) {
+		checks.add(d ?? DateTime.now());
 	}
 
 	// TODO: Remove category
@@ -115,3 +198,4 @@ class Habit {
 		categories.add(cat);
 	}
 }
+
